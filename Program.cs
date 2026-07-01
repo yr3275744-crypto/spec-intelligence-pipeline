fccac05b1017613d99e42d842930601a6ed2a829
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-01 15:46:50 +0300

    add first version of SignalReport

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿namespace IntelligencePipeline;
+﻿using IntelligencePipeline.Models.Reports;
+using System;
+namespace IntelligencePipeline;
+
+class Program
+{
+    static void Main()
+    {
+        SignalReport signal1 = new SignalReport(1, DateTime.Now, 23.0050, 58.0588, "all him", 850, "ioio attack?", Models.Enums.Language.English,-101);
+        signal1.ReliabilityScore = signal1.CalculateReliabilityScore();
+        Console.WriteLine(signal1.ToString());
+    }
+}
