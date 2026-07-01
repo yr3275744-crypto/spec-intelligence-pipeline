76167b2d23f88621fab2e49ef91061d0c15c82b0
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-01 16:35:19 +0300

    middel of base validator

--- Validation/BaseValidator.cs
+++ Validation/BaseValidator.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using IntelligencePipeline.Models.Reports;
+
+namespace IntelligencePipeline.Validation;
+
+abstract class BaseValidator : IValidator
+{
+    public ValidationResult Validate(Report report)
+    {
+
+    }
+
+    protected ValidationResult ValidateCommonFields(Report report)
+    {
+        DateTime minTime = new DateTime(2020, 01, 01);
+        if (report.Timestamp > DateTime.Now || report.Timestamp < minTime)
+        {
+            return ValidationResult.Failure("Invalid DateTime. You must enter between 2020-01-01 to now.");
+        }
+
+        return ValidationResult.Success();
+    }
+
+    protected abstract ValidationResult ValidateSpecificFields(Report report);
+}
